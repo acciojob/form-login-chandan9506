73a2381562d59f1cc15502a5d5e5bd937edfe5b4
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-22 14:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let fname =document.querySelector('#fname').value;
+	let lname =document.querySelector('#lname').value;
+	alert(fname +" "+lname);
 
 }
+
